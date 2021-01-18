1b5bfa74156c9d2d6cf6cdea5a8ca7e919bb7a47
soniaadiningsih <sonia23an>
2021-01-18 18:51:06 +0700

    first commit

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Siswa;
+
+class SiswaController extends Controller
+{
+    // index (menampilkan semua)
+    public function index(){
+        return Siswa::all();
+    }
+
+    // create (menyimpan data)
+    public function create(request $request){
+        $siswa = new Siswa;
+        $siswa->nama = $request->nama;
+        $siswa->alamat = $request->alamat;
+        $siswa->save();
+
+        return "Data Berhasil Masuk";
+    }
+
+    // update (update semua)
+    public function update(request $request, $id){
+        $nama = $request->nama;
+        $alamat = $request->alamat;
+
+        $siswa = Siswa::find($id);
+        $siswa->nama = $nama;
+        $siswa->alamat = $alamat;
+        $siswa->save();
+
+        return "Data Berhasil di Update";
+    }
+
+    // delete (menghapus data)
+    public function delete($id){
+        $siswa = Siswa::find($id);
+        $siswa->delete();
+
+        return "Data Terhapus";
+    }
+
+}
